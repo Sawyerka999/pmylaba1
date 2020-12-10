2732410640d52d44de82638f9e6555612ee3e309
Maksim <Yellown1gga1@gmail.com>
2020-12-10 22:49:20 +0300

    added lab1 and test

---
+++ app/src/main/java/com/example/pmylab1/Utils.java
@@ -0,0 +1,32 @@
+package com.example.pmylab1;
+
+public class Utils {
+    /**
+     * Находит максимальное значение среди переданных
+     * @param values целочимленные значения, среди которых ищется максимальное
+     * @return найденное минимальное значение
+     */
+    public static int max(int ... values) {
+        int max = values[0];
+        for (int i: values) {
+            if (i > max) {
+                max = i;
+            }
+        }
+        return max;
+    }
+    /**
+     * Находит минимальное значение среди переданных
+     * @param values целочисленные значения, среди которых ищется минимальное
+     * @return найденное минимальное значение
+     */
+    public static int min(int ... values) {
+        int min = values[0];
+        for (int i: values) {
+            if (i < min) {
+                min = i;
+            }
+        }
+        return min;
+    }
+}
